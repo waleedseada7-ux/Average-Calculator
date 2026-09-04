aacaf654963df10030f877a7a3ab6d38d388c143
Waleed Wael <waleedseada7@gmail.com>
2026-09-04 16:42:31 +0300

    Add project files.

---
+++ loopsBasics/Program.cs
@@ -0,0 +1,28 @@
+﻿string stringCarrentScore = null;
+int sum = 0;
+int counter = 0;
+int carrentScore;
+do
+{
+    Console.WriteLine("Enter your students score. Enter -1 to finish!");
+
+    stringCarrentScore = Console.ReadLine().Trim();
+    if (int.TryParse(stringCarrentScore, out carrentScore))
+    {
+        if (carrentScore != -1)
+        {
+            sum += carrentScore;
+            counter ++;
+        }
+    }
+    else
+    {
+        Console.WriteLine("Enter the number correctly.");
+    }
+}
+while (carrentScore != -1);
+
+int average = sum / counter;
+
+Console.WriteLine($"The average is: {average}");
+Console.ReadKey();
